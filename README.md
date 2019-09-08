b389709880bf33f5e3285cb768454489709c7326
Jeril Sebastian <seb988@gmail.com>
2019-09-08 21:36:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Espresso
+
+An attempt to write Express JS from scratch using the http module
---
+++ index.js
@@ -0,0 +1,12 @@
+const Espresso = require("./espresso");
+const cors = require('./cors');
+
+const app = new Espresso();
+app.use(cors);
+
+app.use(async (req, res, next) => {
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify({ message: 'Hello with CORS' }));
+});
+
+server = app.listen(3000, () => console.log("Espresso listening on port 3000"));
